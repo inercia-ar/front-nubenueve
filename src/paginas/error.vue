0b06304f9e38ae71a20f1f40e98b4353d89cb779
octantes <82892379+octantes@users.noreply.github.com>
2026-06-13 07:17:02 -0300

    fully restructured

--- src/paginas/error.vue
+++ src/paginas/error.vue
@@ -17,18 +17,18 @@ function pick(...args) {
 onMounted(() => {
   const slots = [
     // zone: [topMin, topMax, leftMin, leftMax, sizeMin, sizeMax, ...colors]
-    { text: 'SEÑAL',   zone: [-5, 2, 0, 20, 14, 20, '--carbonSO', '--carbonSO', '--lirio05'] },
-    { text: 'PERDIDA', zone: [-3, 4, 60, 85, 12, 16, '--lirio05', '--carbonSO', '--carbonSO'] },
-    { text: 'SEÑAL',   zone: [8, 18, 30, 50, 10, 14, '--humo10', '--humo10', '--carbonSO'] },
-    { text: 'PERDIDA', zone: [8, 18, 70, 95, 10, 13, '--carbonSO', '--lirio05', '--humo10'] },
-    { text: 'SEÑAL',   zone: [22, 34, -18, -5, 22, 30, '--carbonSO', '--carbonSO', '--humo10'] },
-    { text: 'PERDIDA', zone: [28, 38, 60, 80, 18, 24, '--lirio10', '--lirio05', '--carbonSO'] },
-    { text: 'SEÑAL',   zone: [40, 55, 25, 45, 14, 18, '--humo10', '--carbonSO', '--lirio05'] },
-    { text: 'PERDIDA', zone: [42, 52, -10, 5, 11, 14, '--lirio05', '--carbonSO', '--humo10'] },
-    { text: 'SEÑAL',   zone: [62, 75, 60, 80, 10, 15, '--humo10', '--carbonSO', '--carbonSO'] },
-    { text: 'PERDIDA', zone: [65, 75, 0, 15, 8, 10, '--carbonSO', '--humo05', '--lirio05'] },
-    { text: 'SEÑAL',   zone: [78, 88, 40, 65, 10, 13, '--carbonSO', '--humo05', '--carbonSO'] },
-    { text: 'PERDIDA', zone: [80, 90, 18, 35, 6, 8, '--humo05', '--carbonSO', '--carbonSO'] },
+    { text: 'SEÑAL',   zone: [-5, 2, 0, 20, 14, 20, '--negro-b-SO', '--negro-b-SO', '--marron-05'] },
+    { text: 'PERDIDA', zone: [-3, 4, 60, 85, 12, 16, '--marron-05', '--negro-b-SO', '--negro-b-SO'] },
+    { text: 'SEÑAL',   zone: [8, 18, 30, 50, 10, 14, '--gris-a-10', '--gris-a-10', '--negro-b-SO'] },
+    { text: 'PERDIDA', zone: [8, 18, 70, 95, 10, 13, '--negro-b-SO', '--marron-05', '--gris-a-10'] },
+    { text: 'SEÑAL',   zone: [22, 34, -18, -5, 22, 30, '--negro-b-SO', '--negro-b-SO', '--gris-a-10'] },
+    { text: 'PERDIDA', zone: [28, 38, 60, 80, 18, 24, '--marron-10', '--marron-05', '--negro-b-SO'] },
+    { text: 'SEÑAL',   zone: [40, 55, 25, 45, 14, 18, '--gris-a-10', '--negro-b-SO', '--marron-05'] },
+    { text: 'PERDIDA', zone: [42, 52, -10, 5, 11, 14, '--marron-05', '--negro-b-SO', '--gris-a-10'] },
+    { text: 'SEÑAL',   zone: [62, 75, 60, 80, 10, 15, '--gris-a-10', '--negro-b-SO', '--negro-b-SO'] },
+    { text: 'PERDIDA', zone: [65, 75, 0, 15, 8, 10, '--negro-b-SO', '--gris-a-05', '--marron-05'] },
+    { text: 'SEÑAL',   zone: [78, 88, 40, 65, 10, 13, '--negro-b-SO', '--gris-a-05', '--negro-b-SO'] },
+    { text: 'PERDIDA', zone: [80, 90, 18, 35, 6, 8, '--gris-a-05', '--negro-b-SO', '--negro-b-SO'] },
   ]
 
   words.value = slots.map(s => {
