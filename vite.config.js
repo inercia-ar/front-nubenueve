d8003c02f7dfb81eda41775385e945c66f7a206a
octantes <82892379+octantes@users.noreply.github.com>
2026-06-15 07:12:57 -0300

    gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 
+  base: '/front-nubenueve/',
   plugins: [ vue() ],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url))} },
   
